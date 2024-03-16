e9c8815c8cbcac7959392d3484027f1ee72d0f8e
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-03-16 15:43:01 +0000

    add error checks

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -23,72 +23,84 @@ const paymasterClient = createPimlicoPaymasterClient({
 })
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-    const body: FrameRequest = await req.json();
-    const { isValid, message } = await getFrameMessage(body, { neynarApiKey: process.env.NEYNAR_API_KEY! });
 
-    if (!isValid) {
-        return new NextResponse('Invalid Frame message', { status: 400 });
-    }
+    try {
 
-    if (!message) {
-        return new NextResponse('Invalid Frame message', { status: 400 });
-    }
 
-    const accountAddress = message.interactor.verified_accounts[0] as Address;
-
-    // send transaction
-    const account = await privateKeyToSafeSmartAccount(publicClient, {
-        privateKey: privateKey as Address,
-        safeVersion: "1.4.1", // simple version
-        entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
-        saltNonce: BigInt(message.interactor.fid)
-    })
-
-    const smartAccountClient = createSmartAccountClient({
-        account,
-        chain: sepolia,
-        transport: http(bundlerUrl),
-        sponsorUserOperation: paymasterClient.sponsorUserOperation,
-    })
-        .extend(bundlerActions)
-        .extend(pimlicoBundlerActions)
+        const body: FrameRequest = await req.json();
+        const { isValid, message } = await getFrameMessage(body, { neynarApiKey: process.env.NEYNAR_API_KEY! });
+    
+        if (!isValid) {
+            return new NextResponse('Invalid Frame message', { status: 400 });
+        }
+    
+        if (!message) {
+            return new NextResponse('Invalid Frame message', { status: 400 });
+        }
+    
+        const accountAddress = message.interactor.verified_accounts[0] as Address;
+    
+        // send transaction
+        const account = await privateKeyToSafeSmartAccount(publicClient, {
+            privateKey: privateKey as Address,
+            safeVersion: "1.4.1", // simple version
+            entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789", // global entrypoint
+            saltNonce: BigInt(message.interactor.fid)
+        })
+    
+        const smartAccountClient = createSmartAccountClient({
+            account,
+            chain: sepolia,
+            transport: http(bundlerUrl),
+            sponsorUserOperation: paymasterClient.sponsorUserOperation,
+        })
+            .extend(bundlerActions)
+            .extend(pimlicoBundlerActions)
+            
+        const gasPrices = await smartAccountClient.getUserOperationGasPrice()
+    
+        const callData = await account.encodeCallData({ 
+            to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045", 
+            data: "0x1234",
+            value: BigInt(0) 
+        })
+    
+        const userOperation = await smartAccountClient.prepareUserOperationRequest({
+            userOperation: {
+                callData
+            },
+        })
+    
+        userOperation.signature = await account.signUserOperation(userOperation)
+    
+        const userOpHash = await smartAccountClient.sendUserOperation({
+            userOperation,
+            entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
+        })
+    
+        return new NextResponse(
+            getFrameHtmlResponse({
+                buttons: [
+                    {
+                        label: `View Smart Account`,
+                        action: "post_redirect"
+                    },
+                ],
+                image: `${NEXT_PUBLIC_URL}/api/og?address=${account.address}&fid=${message.interactor.fid}&userOpHash=${userOpHash}`,
+                post_url: `${NEXT_PUBLIC_URL}/api/etherscan`,
+            }),
+        );
         
-    const gasPrices = await smartAccountClient.getUserOperationGasPrice()
-
-    const callData = await account.encodeCallData({ 
-        to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045", 
-        data: "0x1234",
-        value: BigInt(0) 
-    })
-
-    const userOperation = await smartAccountClient.prepareUserOperationRequest({
-        userOperation: {
-            callData
-        },
-    })
-
-    userOperation.signature = await account.signUserOperation(userOperation)
-
-    const userOpHash = await smartAccountClient.sendUserOperation({
-        userOperation,
-        entryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
-    })
-
-    return new NextResponse(
-        getFrameHtmlResponse({
-            buttons: [
-                {
-                    label: `View Smart Account`,
-                    action: "post_redirect"
-                },
-            ],
-            image: `${NEXT_PUBLIC_URL}/api/og?address=${account.address}&fid=${message.interactor.fid}&userOpHash=${userOpHash}`,
-            post_url: `${NEXT_PUBLIC_URL}/api/etherscan`,
-        }),
-    );
+    
     
 
 
+    }
+
+    catch (err) {
+        console.error(err)
+    }
+   
 }
   
 
