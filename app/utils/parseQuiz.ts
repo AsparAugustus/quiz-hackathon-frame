78a4506d0d0e77e78d478a52272bc65a8019ef07
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-03-16 20:45:21 +0000

    options

--- app/utils/parseQuiz.ts
+++ app/utils/parseQuiz.ts
@@ -15,8 +15,8 @@ export function getOptionsForQuestion(quizData : any, questionIndex : any) {
 
 export function convertOptionstoHTML(options : any, quizNum : any) {
     return options.map((option : any, index : any) => ({
-        label: option.answer,
-        action: 'post_url',
-        target: `${NEXT_PUBLIC_URL}/api/quiz?currentQuestion=${quizNum}&somethingelse=false`,
+        label: option.answer
+        // action: 'post_url',
+        // target: `${NEXT_PUBLIC_URL}/api/quiz?currentQuestion=${quizNum}&somethingelse=false`,
       }));
 }
