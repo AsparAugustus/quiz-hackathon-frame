a1ca39d8bbb4e81bf5e10c5549abd2bf54abd8d0
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2024-03-16 22:29:54 +0000

    probably wont work

--- app/utils/generateSVG.tsx
+++ app/utils/generateSVG.tsx
@@ -1,11 +1,46 @@
 import { ImageResponse } from 'next/og'
 
-export function generateSVG(question : string, options = {}) {
+
+export async function generateSVG(question : string, options = {}) {
+  const imageData = await fetch(
+    new URL('./park-2.png', import.meta.url)
+  ).then((res) => res.arrayBuffer())
+  
+
+  
     const component: JSX.Element = (
-        
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <h1 style={{ fontSize: '48px', margin: 0 }}>{question}</h1>
-        </div>
+        <div
+        style={{
+          display: 'flex',
+          background: '#f6f6f6',
+          width: '100%',
+          height: '100%',
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          position: 'relative',
+        }}
+      >
+        {/* @ts-ignore */}
+        <img width="1200" height="630" alt="meme" src={imageData} />
+        <p
+          style={{
+            position: 'absolute',
+            margin: 0,
+            paddingBottom: 20,
+            color: '#ffffff',
+            lineHeight: 1,
+            fontSize: 100,
+            fontFamily: '"Oswald Bold"',
+            textAlign: 'center',
+            textTransform: 'uppercase',
+            textShadow:
+              '5px 5px 3px #000, -5px 5px 3px #000, -5px -5px 0 #000, 5px -5px 0 #000',
+          }}
+        >
+          {question}
+        </p>
+      </div>
         
       );
 
